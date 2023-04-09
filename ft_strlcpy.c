b508d5d653ccbaa784fae374a0d0f37e7650ac5f
Seokjun Yang <seokjyan@cx2r5s1.42seoul.kr>
2023-04-09 21:07:55 +0900

    20230408_2

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:41:00 by seokjyan          #+#    #+#             */
-/*   Updated: 2023/04/02 18:55:52 by seokjyan         ###   ########.fr       */
+/*   Updated: 2023/04/09 21:07:28 by seokjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 	size_t	src_len;
 
-	i = 0;
 	src_len = ft_strlen(src);
 	if (dstsize == 0)
 		return (src_len);
-	if (!dst || !src)
-		return (0);
+	i = 0;
 	while (i + 1 < dstsize && *(src + i) != '\0')
 	{
 		*(dst + i) = *(src + i);
